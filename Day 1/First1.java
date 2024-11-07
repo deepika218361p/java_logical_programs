47efac736509d65e270efd72bc94d014d6e30955
deepika218361p <bevaradeepika25@gmail.com>
2024-11-07 13:22:31 +0530

    Add files via upload

---
+++ Day 1/First1.java
@@ -0,0 +1,12 @@
+class First1
+{
+public static void main(String args[])
+{
+int empno=102;
+String Empname="Deepika";
+float Empsal=200.56F;
+System.out.println("The given employee number is : "+empno);
+System.out.println("The given employee name is : "+Empname);
+System.out.println("The given employee salary is : "+Empsal);
+}
+}
